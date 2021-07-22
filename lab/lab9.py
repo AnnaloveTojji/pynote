3312dd9ef38866e1749c8cf8e37a66d7eb2268da
Wanseo <wseokim@gmail.com>
2021-07-22 16:51:39 +0900

    Update lab9.py

--- lab/lab9.py
+++ lab/lab9.py
@@ -1,6 +1,6 @@
 #tuples
 
-# Create
+# 1. Create tuples
 # Empty tuple
 my_tuple = ()
 print(my_tuple)
@@ -16,3 +16,139 @@ print(my_tuple)
 # nested tuple
 my_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
 print(my_tuple)
+
+# 2. Tuple Packing
+my_tuple = 3, 4.6, "dog"
+print(my_tuple)
+
+# tuple unpacking is also possible
+a, b, c = my_tuple
+
+print(a)      # 3
+print(b)      # 4.6
+print(c)      # dog
+
+# 3. Tuple with One Element
+my_tuple = ("hello")
+print(type(my_tuple))  # <class 'str'>
+
+# Creating a tuple having one element
+my_tuple = ("hello",)
+print(type(my_tuple))  # <class 'tuple'>
+
+# Parentheses is optional
+my_tuple = "hello",
+print(type(my_tuple))  # <class 'tuple'>
+
+# 4. Indexing
+# Accessing tuple elements using indexing
+my_tuple = ('p','e','r','m','i','t')
+
+print(my_tuple[0])   # 'p' 
+print(my_tuple[5])   # 't'
+
+# IndexError: list index out of range
+# print(my_tuple[6])
+
+# Index must be an integer
+# TypeError: list indices must be integers, not float
+# my_tuple[2.0]
+
+# nested tuple
+n_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+
+# nested index
+print(n_tuple[0][3])       # 's'
+print(n_tuple[1][1])       # 4
+
+# 5. Negative indexing
+# Negative indexing for accessing tuple elements
+my_tuple = ('p', 'e', 'r', 'm', 'i', 't')
+
+# Output: 't'
+print(my_tuple[-1])
+
+# Output: 'p'
+print(my_tuple[-6])
+
+# 6. Slicing
+# Accessing tuple elements using slicing
+my_tuple = ('p','r','o','g','r','a','m','i','z')
+
+# elements 2nd to 4th
+# Output: ('r', 'o', 'g')
+print(my_tuple[1:4])
+
+# elements beginning to 2nd
+# Output: ('p', 'r')
+print(my_tuple[:-7])
+
+# elements 8th to end
+# Output: ('i', 'z')
+print(my_tuple[7:])
+
+# elements beginning to end
+# Output: ('p', 'r', 'o', 'g', 'r', 'a', 'm', 'i', 'z')
+print(my_tuple[:])
+
+#7. Changing
+# Changing tuple values
+my_tuple = (4, 2, 3, [6, 5])
+
+
+# TypeError: 'tuple' object does not support item assignment
+# my_tuple[1] = 9
+
+# However, item of mutable element can be changed
+my_tuple[3][0] = 9    # Output: (4, 2, 3, [9, 5])
+print(my_tuple)
+
+# Tuples can be reassigned
+my_tuple = ('p', 'r', 'o', 'g', 'r', 'a', 'm', 'i', 'z')
+
+# Output: ('p', 'r', 'o', 'g', 'r', 'a', 'm', 'i', 'z')
+print(my_tuple)
+
+# Concatenation
+# Output: (1, 2, 3, 4, 5, 6)
+print((1, 2, 3) + (4, 5, 6))
+
+# Repeat
+# Output: ('Repeat', 'Repeat', 'Repeat')
+print(("Repeat",) * 3)
+
+# 8. Deleting
+# Deleting tuples
+my_tuple = ('p', 'r', 'o', 'g', 'r', 'a', 'm', 'i', 'z')
+
+# can't delete items
+# TypeError: 'tuple' object doesn't support item deletion
+# del my_tuple[3]
+
+# Can delete an entire tuple
+del my_tuple
+
+# NameError: name 'my_tuple' is not defined
+print(my_tuple)
+
+# 9. Tuple methods
+my_tuple = ('a', 'p', 'p', 'l', 'e',)
+
+print(my_tuple.count('p'))  # Output: 2
+print(my_tuple.index('l'))  # Output: 3
+
+# 10. Tuple operations
+# Membership test in tuple
+my_tuple = ('a', 'p', 'p', 'l', 'e',)
+
+# In operation
+print('a' in my_tuple)
+print('b' in my_tuple)
+
+# Not in operation
+print('g' not in my_tuple)
+
+# 11. Iterating through a Tuple
+# Using a for loop to iterate through a tuple
+for name in ('John', 'Kate'):
+    print("Hello", name)
