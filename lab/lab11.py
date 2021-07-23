f71be259d6fc77573363f03cd95c5d7ef3eb6f52
Wanseo <wseokim@gmail.com>
2021-07-23 19:18:27 +0900

    lab11

--- lab/lab11.py
+++ lab/lab11.py
@@ -1,1 +1,150 @@
-# dic
+# sets
+
+# creating sets
+# Different types of sets in Python
+# set of integers
+my_set = {1, 2, 3}
+print(my_set)
+
+# set of mixed datatypes
+my_set = {1.0, "Hello", (1, 2, 3)}
+print(my_set)
+
+# set cannot have duplicates
+# Output: {1, 2, 3, 4}
+my_set = {1, 2, 3, 4, 3, 2}
+print(my_set)
+
+# we can make set from a list
+# Output: {1, 2, 3}
+my_set = set([1, 2, 3, 2])
+print(my_set)
+
+# set cannot have mutable items
+# here [3, 4] is a mutable list
+# this will cause an error.
+
+my_set = {1, 2, [3, 4]}
+
+# Distinguish set and dictionary while creating empty set
+
+# initialize a with {}
+a = {}
+
+# check data type of a
+print(type(a))
+
+# initialize a with set()
+a = set()
+
+# check data type of a
+print(type(a))
+
+# modyfying
+
+# initialize my_set
+my_set = {1, 3}
+print(my_set)
+
+# my_set[0]
+# if you uncomment the above line
+# you will get an error
+# TypeError: 'set' object does not support indexing
+
+# add an element
+# Output: {1, 2, 3}
+my_set.add(2)
+print(my_set)
+
+# add multiple elements
+# Output: {1, 2, 3, 4}
+my_set.update([2, 3, 4])
+print(my_set)
+
+# add list and set
+# Output: {1, 2, 3, 4, 5, 6, 8}
+my_set.update([4, 5], {1, 6, 8})
+print(my_set)
+
+# removing elements
+
+# Difference between discard() and remove()
+
+# initialize my_set
+my_set = {1, 3, 4, 5, 6}
+print(my_set)
+
+# discard an element
+# Output: {1, 3, 5, 6}
+my_set.discard(4)
+print(my_set)
+
+# remove an element
+# Output: {1, 3, 5}
+my_set.remove(6)
+print(my_set)
+
+# discard an element
+# not present in my_set
+# Output: {1, 3, 5}
+my_set.discard(2)
+print(my_set)
+
+# remove an element
+# not present in my_set
+# you will get an error.
+# Output: KeyError
+
+my_set.remove(2)
+
+# initialize my_set
+# Output: set of unique elements
+my_set = set("HelloWorld")
+print(my_set)
+
+# pop an element
+# Output: random element
+print(my_set.pop())
+
+# pop another element
+my_set.pop()
+print(my_set)
+
+# clear my_set
+# Output: set()
+my_set.clear()
+print(my_set)
+
+print(my_set)
+
+# set operations
+
+# Set union method
+# initialize A and B
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+# use | operator
+# Output: {1, 2, 3, 4, 5, 6, 7, 8}
+print(A | B)
+
+# set difference
+
+# Difference of two sets
+# initialize A and B
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+# use - operator on A
+# Output: {1, 2, 3}
+print(A - B)
+
+# symmetric difference
+# Symmetric difference of two sets
+# initialize A and B
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+# use ^ operator
+# Output: {1, 2, 3, 6, 7, 8}
+print(A ^ B)
