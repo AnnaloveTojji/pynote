ce9c82a5c5f75ed0e167b596f40bbda0ab30a318
Wanseo <wseokim@gmail.com>
2021-08-07 13:27:13 +0900

    lab

---
+++ lab/loops/for.py
@@ -0,0 +1,66 @@
+# loops
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)  
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+
+  # Program to find the sum of all numbers stored in a list
+
+# List of numbers
+numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
+
+# variable to store the sum
+sum = 0
+
+# iterate over the list
+for val in numbers:
+    sum = sum+val
+
+print("The sum is", sum)
+
+print(range(10))
+
+print(list(range(10)))
+
+print(list(range(2, 8)))
+
+print(list(range(2, 20, 3)))
+
+# Program to iterate through a list using indexing
+
+genre = ['pop', 'rock', 'jazz']
+
+# iterate over the list using index
+for i in range(len(genre)):
+    print("I like", genre[i])
+
+digits = [0, 1, 5]
+
+for i in digits:
+    print(i)
+else:
+    print("No items left.")
+
+# program to display student's marks from record
+student_name = 'Soyuj'
+
+marks = {'James': 90, 'Jules': 55, 'Arthur': 77}
+
+for student in marks:
+    if student == student_name:
+        print(marks[student])
+        break
+else:
+    print('No entry with that name found.')
