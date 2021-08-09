5878886946344282ff25627c08d0d5b3aa75a8d5
Wanseo <wseokim@gmail.com>
2021-08-09 23:04:59 +0900

    update

---
+++ lab/inheritence/class1.py
@@ -0,0 +1,37 @@
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+print(p1.x)
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
