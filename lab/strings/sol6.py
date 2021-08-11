09106798bd73dbba4f51471d6c5ddd23068f223a
Wanseo <wseokim@gmail.com>
2021-08-11 13:47:19 +0900

    Update sol6.py

--- lab/strings/sol6.py
+++ lab/strings/sol6.py
@@ -1,5 +1,14 @@
 #bj 1316: group word checker
-n = int(input())
-cnt = 0
-for i in range(n):
-    if 
+# method1
+
+# method2: slicing
+n=int(input())
+cnt=0
+for j in range(n):
+    word=input()
+    for i in range(len(word)-1):
+        if word[i]!=word[i+1]:
+            if word[i] in word[i+1:]:
+                n-=1
+                break
+print(n)
