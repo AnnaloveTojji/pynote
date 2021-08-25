a145c7b068dba76e302d61e223cc70ed5cb6c4d1
Wanseo <wseokim@gmail.com>
2021-08-25 20:48:20 +0900

    update

--- tkinter/tk_BrickBreaker.py
+++ tkinter/tk_BrickBreaker.py
@@ -4,7 +4,7 @@ import time
  
 tk = Tk()
 tk.title("Game")
-tk.resizable(0, 0)
+tk.resizable(0, 0) 
 tk.wm_attributes("-topmost", 1)
 canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
--- tkinter/tk_calculator.py
+++ tkinter/tk_calculator.py
@@ -26,20 +26,20 @@ def button_equal():
     e.delete(0,END)
     e.insert(0,f_num + int(second_number))
 
-button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
-button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
-button_3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
-button_4 = Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4))
-button_5 = Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5))
-button_6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6))
-button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
-button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
-button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
-button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
+button_1 = Button(root, text="1", padx=40, pady=20, highlightbackground='#0000ff', command=lambda: button_click(1))
+button_2 = Button(root, text="2", padx=40, pady=20, highlightbackground='#0000ff', command=lambda: button_click(2))
+button_3 = Button(root, text="3", padx=40, pady=20, highlightbackground='#0000ff', command=lambda: button_click(3))
+button_4 = Button(root, text="4", padx=40, pady=20, highlightbackground='#0000ff', command=lambda: button_click(4))
+button_5 = Button(root, text="5", padx=40, pady=20, highlightbackground='#0000ff', command=lambda: button_click(5))
+button_6 = Button(root, text="6", padx=40, pady=20, highlightbackground='#0000ff', command=lambda: button_click(6))
+button_7 = Button(root, text="7", padx=40, pady=20, highlightbackground='#0000ff', command=lambda: button_click(7))
+button_8 = Button(root, text="8", padx=40, pady=20, highlightbackground='#0000ff', command=lambda: button_click(8))
+button_9 = Button(root, text="9", padx=40, pady=20, highlightbackground='#0000ff', command=lambda: button_click(9))
+button_0 = Button(root, text="0", padx=40, pady=20, highlightbackground='#0000ff', command=lambda: button_click(0))
 
-button_add = Button(root,text="+",padx=39, pady=20,command=button_add)
-button_equal = Button(root,text="=",padx=39, pady=20,command=button_equal)
-button_clear = Button(root,text="Clear",padx=39, pady=20,command=button_clear)
+button_add = Button(root,text="+",padx=39, pady=20, highlightbackground='#0000ff', command=button_add)
+button_equal = Button(root,text="=",padx=39, pady=20, highlightbackground='#0000ff', command=button_equal)
+button_clear = Button(root,text="Clear",padx=39, pady=20, highlightbackground='#0000ff', command=button_clear)
 
 
 # put the buttons on the screen
