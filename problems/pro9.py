46c5d7a47b4d91ba9d3697ce622b8833b0907b90
Wanseo <wseokim@gmail.com>
2021-10-30 13:45:50 +0900

    update

--- problems/pro9.py
+++ problems/pro9.py
@@ -1,1 +1,15 @@
 #heap 더맵게
+	def solution(scoville, K):
+    import heapq
+    answer = 0
+    heapq.heapify(scoville) #list to heapq
+    
+    while scoville[0] < K: #가장 작은 수가 기준보다 낮다면 계속
+        if len(scoville)>1:
+            answer += 1
+            first = heapq.heappop(scoville) 
+            second = heapq.heappop(scoville) 
+            heapq.heappush(scoville, first + second *2)
+        else:
+            return -1
+    return answer
