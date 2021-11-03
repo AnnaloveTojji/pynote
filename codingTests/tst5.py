52db33699b31375b7d678449b0c082c51706f0d9
Wanseo <wseokim@gmail.com>
2021-11-03 19:08:07 +0900

    Update tst5.py

--- codingTests/tst5.py
+++ codingTests/tst5.py
@@ -1,15 +1,7 @@
 #publy3 -3
 # unattacking rooks
 
-for (int i = 0; i < boardSize; i++){
-    board[col][i] = 1;
-    if (canPlace(col, i, board)) {
-        nsolutions += findNumSolutions(board, col + 1, rooksLeft - 1);
-        board[col][i] = 0;
-    }
- }
-
- def solution(n, k):
+def solution(n, k):
     answer = -1
     res=n
 
@@ -28,7 +20,5 @@ for (int i = 0; i < boardSize; i++){
         ri += 1
         ci += 1
         res -= 1
-    
-    
-    
+
     return answer
