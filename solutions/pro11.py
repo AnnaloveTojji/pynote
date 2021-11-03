d723b19f6a06800ba07afb08c6e4dc50a55d1c59
Wanseo <wseokim@gmail.com>
2021-11-03 17:33:24 +0900

    folder name

---
+++ solutions/pro11.py
@@ -0,0 +1,15 @@
+#큰수만들기-greedy
+
+def solution(number, k):
+    collected = []
+    for i, num in enumerate(number):
+        while len(collected) > 0 and collected[-1] < num and k > 0:
+            collected.pop()
+            k -= 1
+        if k == 0:
+            collected += list(number[i:])
+            break
+        collected.append(num)
+
+        return answer
+        
