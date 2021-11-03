92e4216deb19aece4617ec303f720f6d88e0a23f
Wanseo <wseokim@gmail.com>
2021-11-03 14:18:07 +0900

    update

--- problems/pro3.py
+++ problems/pro3.py
@@ -1,1 +1,9 @@
 #가장큰수
+def solution(numbers):
+    numbers = [str(x) for x in numbers]
+    numbers.sort(key=lambda x : (x * 4)[:4], reverse=True)
+    if numbers[0] == '0':
+        answer = '0'
+    else:
+        answer = ''.join(numbers)
+    return answer
---
+++ problems/pro8.py
@@ -0,0 +1,19 @@
+# 더맵게
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while True:
+        min1 = heapq.heappop(scoville)
+        if min1 >= K:
+            break
+        elif len(scoville) == 0:
+            answer = -1
+            break
+        min2 = heapq.heappop(scoville)
+        new_scoviile = min1 + 2 * min2
+        heapq.heappush(scoville, new_scoville)
+        answer += 1
+    return answer
+    
